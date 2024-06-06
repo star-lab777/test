d58b9562071974d51126802aaa6180bc81223ffb
asish <starasish08@gmail.com>
2024-06-06 13:44:30 +0000

    js-file

---
+++ react.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Box } from "rebass";
+
+const gfg = (props) => {
+	return (
+		<div id="gfg">
+			<br />
+			<Box
+				p={5}
+				fontSize={5}
+				width={[0.5]}
+				color="white"
+				bg="Green"
+				textAlign="center"
+			>
+				GeeksforGeeks
+			</Box>
+		</div>
+	);
+};
+
+export default gfg;
+
